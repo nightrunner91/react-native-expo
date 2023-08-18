58d6d28bc0b29533d030088fa90debf4a21f302d
Nikita <semikin91@gmail.com>
2023-08-18 14:39:36 +0300

    Added axios as a test 3rd party plugin

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React!</Text>
+      <Text>Hello React!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
